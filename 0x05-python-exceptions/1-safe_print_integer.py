c0157f6f8a858bd155acb63155eaf2430d2531c9
gwagsi <gwagsiglenn@gmail.com>
2023-12-19 12:00:53 -0800

    1-mai

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#1-safe_print_integer.py
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
